00676ab07502b612ed40aec1233d4c520aacf1f0
Azad500 <azad.mirzazada95@gmail.com>
2024-03-02 00:02:38 +0400

    finished

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger-menu");
   const navLinksContainer = document.querySelector(".nav-links-container");
   const xIconElement = document.querySelector(".x-icon");
+  const removePage = document.querySelectorAll(".removePage");
 
   hamburger.addEventListener("click", function () {
     navLinksContainer.classList.toggle("active");
@@ -10,11 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
   xIconElement.addEventListener("click", function () {
     navLinksContainer.classList.remove("active");
   });
+  removePage.forEach(function (element) {
+    element.addEventListener("click", function () {
+      navLinksContainer.classList.remove("active");
+    });
+  });
 });
 // ---------------------animate-slide-down---------------------------
 document.addEventListener("DOMContentLoaded", function () {
   const elementsToAnimate = document.querySelectorAll(
-    ".home-container div, .home-container h2, .home-container p, .about-container div, .about-container h2, .about-container p,.truck-cargo-element h2, .truck-cargo-element p"
+    ".home-container div, .home-container h2, .home-container p, .about-container h2, .about-container p,.truck-cargo-element h2, .truck-cargo-element p,.truck-element h2,.truck-element p,.services-element h2,.services-element h3,.services-element p,.customer-element h2,.customer-element p,.contact-container h2,.contact-container p,.contact-container img"
   );
   elementsToAnimate.forEach(function (element) {
     element.classList.add("animate-slide-down");
