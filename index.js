634b5aa038a6f434da295d4eca788e3ced2885bd
Azad500 <azad.mirzazada95@gmail.com>
2024-03-04 09:10:16 +0400

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // ---------------------animate-slide-down---------------------------
 document.addEventListener("DOMContentLoaded", function () {
   const elementsToAnimate = document.querySelectorAll(
-    ".home-container div, .home-container h2, .home-container p, .about-container h2, .about-container p,.truck-cargo-element h2, .truck-cargo-element p,.truck-element h2,.truck-element p,.services-element h2,.services-element h3,.services-element p,.customer-element h2,.customer-element p,.contact-container h2,.contact-container p,.contact-container img"
+    ".home-container div, .home-container h2, .home-container p,.about-us-button, .about-container h2, .about-container p,.truck-cargo-element h2, .truck-cargo-element p,.truck-element h2,.truck-element p,.services-element h2,.services-element h3,.services-element p,.customer-element h2,.customer-element p,.contact-container h2,.contact-container p,.contact-container img"
   );
   elementsToAnimate.forEach(function (element) {
     element.classList.add("animate-slide-down");
